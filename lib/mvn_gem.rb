8a6e4c794b755b87fbe2b5272a00197e99d9458b
David Calavera <david.calavera@gmail.com>
2009-12-17 13:43:55 +0100

    ability to create a gem rather than install

--- lib/mvn_gem.rb
+++ lib/mvn_gem.rb
@@ -5,25 +5,24 @@ require 'rubygems/gem_runner'
 module MavenGem
   def MavenGem.install(group, artifact = nil, version = nil)
     begin
-      if artifact
-        # fetch pom and install
-        url = MavenGem::PomSpec.maven_base_url + "/#{group}/#{artifact}/#{version}/#{artifact}-#{version}.pom"
-        gem = PomSpec.from_url(url)
-      else
-        if group =~ %r[^http://]
-          gem = PomSpec.from_url(pom_location)
-        else
-          gem = PomSpec.from_file(pom_location)
-        end
-      end
+      gem = build(group, artifact, version)
       Gem::GemRunner.new.run(["install", gem])
     ensure
       FileUtils.rm_f gem
     end
   end
-end
 
-case ARGV.shift
-when 'install'
-  MavenGem.install *ARGV
+  def MavenGem.build(group, artifact = nil, version = nil)
+    gem = if artifact
+      # fetch pom and install
+      url = MavenGem::PomSpec.maven_base_url + "/#{group}/#{artifact}/#{version}/#{artifact}-#{version}.pom"
+      PomSpec.from_url(url)
+    else
+      if group =~ %r[^http://]
+        PomSpec.from_url(group)
+      else
+        PomSpec.from_file(group)
+      end
+    end
+  end
 end
